800c407f85f416e25a227ea9d7fd5ecd8bbfa6d0
Justin C. Bagley <centrarchidae@gmail.com>
2018-02-21 16:53:28 -0500

    Updated several parts of script

--- fastq_readLengthChecker.sh
+++ fastq_readLengthChecker.sh
@@ -32,8 +32,8 @@ fi
 	done 
 )
 
-NUM_FASTQS="$(wc -l ./names.txt | sed 's/\ \.\/names\.txt//g')"
-echo "INFO      | $(date) |          Found $NUM_FASTQS .fastq files in current working directory. "
+NUM_FASTQS="$(wc -l ./names.txt | sed 's/\ \.\/names\.txt//g' | sed 's/\ //g')"
+echo "INFO      | $(date) | Found $NUM_FASTQS .fastq files in current working directory. "
 
 paste ./names.txt ./lengths.txt > fastq_lengths_summary.tmp
 
@@ -42,13 +42,13 @@ echo "file	read_length" > header.tmp
 cat ./header.tmp ./fastq_lengths_summary.tmp > ./fastq_lengths_summary.txt
 
 SHORTEST_READS="$(cut -f2 -d$'\t' fastq_lengths_summary.txt | sort -n | head -2 | tail -n1 | sed 's/\ //g')"
-LONGEST_READS="$(cut -f2 -d$'\t' fastq_lengths_summary.txt | sort -n | head -2 | tail -n1 | sed 's/\ //g')"
+LONGEST_READS="$(cut -f2 -d$'\t' fastq_lengths_summary.txt | sort -n | tail -1 | tail -n1 | sed 's/\ //g')"
 
-echo "INFO      | $(date) |          Shortest read length: $SHORTEST_READS "
-echo "INFO      | $(date) |          Longest read length: $LONGEST_READS "
-echo "INFO      | $(date) |          Results output to 'fastq_lengths_summary.txt' in current working directory."
+echo "INFO      | $(date) | Shortest read length: $SHORTEST_READS "
+echo "INFO      | $(date) | Longest read length: $LONGEST_READS "
+echo "INFO      | $(date) | Results output to 'fastq_lengths_summary.txt' in current working directory."
 
-echo "INFO      | $(date) |          Cleaning up workspace... "
+echo "INFO      | $(date) | Cleaning up workspace... "
 rm ./header.tmp ./fastq_lengths_summary.tmp ./names.txt ./lengths.txt
 
 echo "INFO      | $(date) | Done checking fastq read lengths. "
